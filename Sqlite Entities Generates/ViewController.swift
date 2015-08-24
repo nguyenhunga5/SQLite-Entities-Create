e64d001edf2e21c60b45dc7425335a9d18c93c2e
Nguyen Thanh Hung1 <hungnt1@evolableasia.vn>
2015-08-24 08:55:49 +0700

    Initial Commit

---
+++ Sqlite Entities Generates/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sqlite Entities Generates
+//
+//  Created by Nguyen Thanh Hung on 8/24/15.
+//  Copyright (c) 2015 Nguyen Thanh Hung. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Sqlite Entities GeneratesTests/Sqlite_Entities_GeneratesTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sqlite_Entities_GeneratesTests.swift
+//  Sqlite Entities GeneratesTests
+//
+//  Created by Nguyen Thanh Hung on 8/24/15.
+//  Copyright (c) 2015 Nguyen Thanh Hung. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class Sqlite_Entities_GeneratesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
